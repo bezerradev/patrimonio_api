90a4bf5d79d24879c7d88df6256fcc2bae767c38
Mateus Bezerra <bezerra.dev@gmail.com>
2019-08-16 03:17:10 -0300

    primeira tentativa deploy heroku

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ import uuid
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
-
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5433/patri"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 db = SQLAlchemy(app)
 
